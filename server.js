b9af8d262abd776a3e2d399a295e287952986ba5
rediet yilma <rediyilma75@gmail.com>
2026-04-07 14:04:26 +0300

    git didt

--- server.js
+++ server.js
@@ -51,7 +51,8 @@ app.use(compression());
 app.use(express.json());
 const allowedOrigins = [
   "https://weddinginvitation.newblossomequb.net", // correct frontend domain
-  "http://localhost:5173"                           // local dev
+  "http://localhost:5173" ,
+  "https://paulandhella.newblossomequb.net"                          // local dev
 ];
 app.use(cors({
   origin: function(origin, callback) {
